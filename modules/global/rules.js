b05a7215297db033a32efc8410ded2e358fb3dcf
NeaByteLab <209737579+NeaByteLab@users.noreply.github.com>
2025-07-05 00:12:55 +0700

    feat(routes): update general routes, refactor server, remove example route, and add modular routing :triangular_flag_on_post:

---
+++ modules/global/rules.js
@@ -0,0 +1,11 @@
+const { field } = require('../../validator/index')
+
+/**
+ * Validation Rules
+ * - This module defines validation rules for API endpoints.
+ * - It includes rules for string length, email format, and sanitization.
+ */
+module.exports = [
+  field('name').isString().length({ min: 3, max: 30 }).sanitize(),
+  field('email').isEmail().length({ min: 5, max: 50 }).sanitize()
+]
