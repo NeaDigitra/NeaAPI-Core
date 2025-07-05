95e50929cc6c7c98883c280dd9a8292c18c0eab5
NeaByteLab <209737579+NeaByteLab@users.noreply.github.com>
2025-07-05 07:44:07 +0700

    feat(config): add rate limit config file 📄

---
+++ src/config/ratelimit.js
@@ -0,0 +1,10 @@
+/**
+ * Rate Limit Config
+ * - This module exports the rate limit settings for the application.
+ * - It uses environment variables to set the maximum number of requests and the time window for rate
+ */
+require('dotenv').config({ debug: process.env.APP_DEBUG === 'true' })
+module.exports = {
+  maxRequests: parseInt(process.env.RATE_LIMIT_MAX, 10) || 100,
+  windowSeconds: parseInt(process.env.RATE_LIMIT_WINDOW, 10) || 60,
+}
