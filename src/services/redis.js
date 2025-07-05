81e0aad1833e1f0b5cac21795b5ea0b8851e1dea
NeaByteLab <209737579+NeaByteLab@users.noreply.github.com>
2025-07-05 08:15:18 +0700

    chore(redis): add redis client error and ready handlers 🛠

--- src/services/redis.js
+++ src/services/redis.js
@@ -1,15 +1,25 @@
+const { appName, appVersion, appPort, errorBaseUrl } = require('config/app')
 const redis = require('redis')
 
 /**
  * Redis Client Configuration
  * - This module exports a Redis client instance configured with environment variables.
- * - It connects to a Redis server using the URL and password specified in the environment variables.
  */
 const redisClient = redis.createClient({
   url: process.env.REDIS_URL || 'redis://localhost:6379',
   password: process.env.REDIS_PASSWORD || undefined
 })
-redisClient.connect()
+
+/**
+ * Event Listeners
+ * - Handles connection errors and logs when the client is ready.
+ * - This is crucial for debugging and ensuring the Redis client is operational.
+ */
+redisClient.on('error', (err) => {
+  console.error(`[${appName}-Redis] Client Error:`, err.stack)
+}).on('ready', () => {
+  console.log(`[${appName}-Redis] Client Connected Successfully`)
+}).connect()
 
 /**
  * Exports
